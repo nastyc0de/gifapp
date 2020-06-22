cf5bf2ec9e5eb00ef35d8a380463b0269f4ab06f
Andres <nastybitsz@gmail.com>
2020-06-22 15:09:30 -0400

    docs agregado del curso de fernando

---
+++ docs/precache-manifest.eb716253672f839e33dba53082ae0017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583fbe03957b4a007b1fe7be08adfa68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe8ffb96e5f6000c531e",
+    "url": "/static/css/main.5ccab562.chunk.css"
+  },
+  {
+    "revision": "f4f94b6f63ef6d271e4b",
+    "url": "/static/js/2.527f945a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.527f945a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe8ffb96e5f6000c531e",
+    "url": "/static/js/main.9cd42df6.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
